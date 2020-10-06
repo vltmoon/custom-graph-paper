143b168d76971d867d7664d474572ab29841b048
Violet Moon <violet.wmoon@gmail.com>
2020-10-06 13:36:40 -0500

    color picker floats and closes on outside click

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,30 +1,53 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect, useCallback, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { SketchPicker } from 'react-color'
 import { PaperContext } from './Paper'
 
 export default function Column() {
     const { numberOfRows, numberOfCols } = useContext(PaperContext)
+    const colorPickerRef = useRef()
 
-    const [isColorPickerOpen, toggleColorPicker] = useState(false)
+    const [isColorPickerOpen, openColorPicker] = useState(false)
     const [color, setColor] = useState(false)
+
+    const toggleColorPicker = () => {
+        if (!isColorPickerOpen) {
+            openColorPicker(true)
+        }
+    }
+
+    const closeColorPicker = useCallback((event) => {
+        if (!colorPickerRef.current?.contains(event.target)) {
+            openColorPicker(false)
+        }
+    }, [colorPickerRef])
+
+    useEffect(() => {
+        document.addEventListener('mousedown', closeColorPicker);
+        return () => document.removeEventListener('mousedown', closeColorPicker);
+    });
     
     return (
-        <React.Fragment>
+        <div style={col} ref={colorPickerRef}>
             <div
-                style={{...Col, backgroundColor: color.hex}}
-                onClick={() => toggleColorPicker(!isColorPickerOpen)}
+                style={{...fill, backgroundColor: color.hex}}
+                onClick={toggleColorPicker}
             />
             {isColorPickerOpen && (
                 <SketchPicker color={color} onChange={setColor} />
             )}
-        </React.Fragment>
+        </div>
     )
 
 }
 
-const Col = {
+const col = {
     height: 20,
     width: 20,
     border: '.1px solid black'
+}
+
+const fill = {
+    width: '100%',
+    height: '100%',
 }
