257e65060b349b9c340ac988429114b052fed612
Violet Moon <violet.wmoon@gmail.com>
2020-10-06 14:31:40 -0500

    added color swatch

--- src/components/Paper.jsx
+++ src/components/Paper.jsx
@@ -12,7 +12,7 @@ export default function Paper() {
 
     const [usedColors, setUsedColors] = useState([])
     const [colorPickerOpen, toggleColorPicker] = useState(false)
-    const [groupApply, setGroupApply] = useState({ color: '#fff', enable: false })
+    const [groupApply, setGroupApply] = useState({ color: { hex: '#000' }, enable: false })
 
     
     const contextValue = {
@@ -32,21 +32,35 @@ export default function Paper() {
 
     return (
         <PaperContext.Provider value={contextValue}>
+            {/* Graph paper layout */}
             <div style={paperContainer}>
                 <Rows />
             </div>
             
             <div>{usedColors}</div>
             
-            {/* Bulk apply colors to grid without having to toggle each item */}
-            <input type='checkbox' id='groupApply' value={groupApply} onClick={enableGroupApply}/>
-            <label for='groupApply'>Group color apply</label>
+            <div>
+                {/* Bulk apply colors to grid without having to toggle each item */}
+                <input type='checkbox' id='groupApply' value={groupApply} onClick={enableGroupApply}/>
+                <label for='groupApply'>Enable group color apply</label>
 
+                <div style={{ display: 'flex', marginTop: '10px'}}>
+                    {/* Set bulk apply color with sample swatch */}
+                    <div style={swatchContainer}>
+                        <div style={{ ...sampleSwatch, backgroundColor: groupApply.color.hex }}/>
+                    </div>
+                    <button
+                        style={{ marginLeft: '10px'}}
+                        disabled={!groupApply.enable}
+                        onClick={() => toggleColorPicker(!colorPickerOpen)}>
+                        Select group color
+                    </button>
+                </div>
 
-            <button onClick={() => toggleColorPicker(!colorPickerOpen)}>select group color</button>
-            {colorPickerOpen && (
-                <ChromePicker color={groupApply.color} onChange={setGroupApplyColor} />
-            )}
+                {colorPickerOpen && (
+                    <ChromePicker color={groupApply.color} onChange={setGroupApplyColor} />
+                )}
+            </div>
         </PaperContext.Provider>
     );
 }
@@ -54,6 +68,19 @@ export default function Paper() {
 const paperContainer = {
     display: 'flex',
     flexFlow: 'column nowrap'
+}
+
+const swatchContainer = {
+    height: 20,
+    width: 40,
+    boxShadow: '1px 1px 5px 1px rgba(0, 0, 0, .2)',
+    border: '5px solid white',
+}
+
+const sampleSwatch = {
+    borderRadius: '3px',
+    height: '100%',
+    width: '100%',
 }
 
 Paper.propTypes = {
