5315a7530ee96af48c1ddbd8ec71f8e72db1a1bf
Violet Moon <violet.wmoon@gmail.com>
2020-10-06 12:20:46 -0500

    removed default react app filler

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+import reducer from './store/reducers'
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  	<Provider store={store}>
+	    <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
