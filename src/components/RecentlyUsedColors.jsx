86e8379594691b585d3d3f6552651d5a80df16a9
Violet Moon <violet.wmoon@gmail.com>
2020-10-07 22:04:00 -0500

    moved recent colors to sep comp

---
+++ src/components/RecentlyUsedColors.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { GridDesignerContext } from './Paper'
+
+export default function RecentlyUsedColors() {
+    const { usedColors } = useContext(GridDesignerContext)
+
+    return (
+        <React.Fragment>
+            <div>Recent colors:</div>
+            {usedColors.map((color, i) => {
+                return color.hex && (
+                    <div key= {i} style={swatchContainer}>
+                        <div style={{ ...sampleSwatch, backgroundColor: color.hex }}>
+                            <div style={{ color: 'white'}}>{color.hex}</div>
+                        </div>
+                    </div>
+                )
+            })}
+        </React.Fragment>
+    )
+}
+
+const swatchContainer = {
+    height: 20,
+    boxShadow: '1px 1px 5px 1px rgba(0, 0, 0, .2)',
+    border: '3px solid white',
+}
+
+const sampleSwatch = {
+    borderRadius: '3px',
+    height: '100%',
+    width: '100%',
+}
