3d9f275f2feb8c54d9bad4e6173f20bf79269234
Violet Moon <violet.wmoon@gmail.com>
2020-10-23 20:09:49 -0500

    added style file

---
+++ src/styles/jss/global.js
@@ -0,0 +1,55 @@
+export const primaryFont = 'Commissioner'
+export const secondaryFont = 'Beth Ellen'
+
+export smallFontSize = '11px'
+export normalFontSize = '13px'
+export largeFontSize = '40px'
+
+export const elementSpacing = '20px'
+
+export const primaryColor = '#E8D9D9'
+export const secondaryColor = '#584949'
+export const tertiaryColor = '#e0e0e0'
+
+
+
+
+#1a4a52
+#bb6b55
+#ecd6d0
+
+#eae4bd
+#ece9d4
+#F1E38B
+#f1e38b
+#dabf12
+#ac9600
+
+#e4b3b3
+#ecd6d0
+#bb6b55
+#521606
+
+#3d1640
+#635279
+#554965
+#8d8696
+#b9b2c3
+
+
+#8a987b
+#afb5a8
+#818879
+#506933
+#3b5f11
+#38521a
+
+#1a4a52
+#23385d
+#bccdea
+#848e9e
+#4d5461
+#546992
+#4a71bd
+#33518c
+#162440
